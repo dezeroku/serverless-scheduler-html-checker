c1ecd3739b1c1b33a5fcfd48394b43ac9029c033
dezeroku <dezeroku@gmail.com>
2023-03-09 04:48:57 +0100

    handler: init

--- serverless_scheduler_html_checker/handler.py
+++ serverless_scheduler_html_checker/handler.py
@@ -2,6 +2,10 @@ from typing import TYPE_CHECKING
 
 from serverless_scheduler_html_checker_api.models.html_monitor_job import HTMLMonitorJob
 
+from serverless_scheduler_html_checker.bucket_state import get_state, write_state
+from serverless_scheduler_html_checker.html import get_html
+from serverless_scheduler_html_checker.ses import send_email
+
 if TYPE_CHECKING:
     from mypy_boto3_s3 import S3Client
     from mypy_boto3_ses import SESClient
@@ -15,5 +19,21 @@ def entrypoint(event, context):
     pass
 
 
-def handler(records: list[HTMLMonitorJob], s3_client: S3Client, ses_client: SESClient):
-    pass
+def handler(
+    records: list[HTMLMonitorJob],
+    s3_client: S3Client,
+    bucket_name: str,
+    ses_client: SESClient,
+    template_name: str,
+    source_email: str,
+):
+    for rec in records:
+        state = get_html(rec.url)
+        path = rec.get_unique_job_id()
+        if (old_state := get_state(s3_client, bucket_name, path)) is not None:
+            if state != old_state:
+                send_email(
+                    ses_client, template_name, source_email, [rec.user_email], rec.url
+                )
+
+        write_state(s3_client, bucket_name, path, state)
---
+++ tests/test_handler.py
@@ -0,0 +1,173 @@
+from common.models.plugins import parse_dict_to_job
+from mypy_boto3_s3 import S3Client
+from mypy_boto3_ses import SESClient
+
+from serverless_scheduler_html_checker.handler import handler
+
+
+def common_setup(
+    helpers,
+    mock_s3,
+    mock_ses,
+    example_bucket_name,
+    example_template_name,
+    example_source_email,
+):
+    helpers.empty_mock_bucket(mock_s3, example_bucket_name)
+    helpers.create_ses_template(mock_ses, example_template_name)
+    helpers.verify_ses_email(mock_ses, example_source_email)
+
+
+def test_handler_no_records(
+    helpers,
+    mock_s3: S3Client,
+    mock_ses: SESClient,
+    example_bucket_name,
+    example_template_name,
+    example_source_email,
+):
+    common_setup(
+        helpers,
+        mock_s3,
+        mock_ses,
+        example_bucket_name,
+        example_template_name,
+        example_source_email,
+    )
+
+    handler(
+        [],
+        mock_s3,
+        example_bucket_name,
+        mock_ses,
+        example_template_name,
+        example_source_email,
+    )
+
+    assert int(mock_ses.get_send_quota()["SentLast24Hours"]) == 0
+
+
+def test_handler_no_previous_state(
+    helpers,
+    mock_s3: S3Client,
+    mock_ses: SESClient,
+    example_bucket_name,
+    example_template_name,
+    example_source_email,
+    requests_mock,
+):
+    common_setup(
+        helpers,
+        mock_s3,
+        mock_ses,
+        example_bucket_name,
+        example_template_name,
+        example_source_email,
+    )
+
+    job = parse_dict_to_job(helpers.html_monitor_job_dict_factory())
+
+    data = "test"
+    requests_mock.get(job.url, text=data)
+
+    handler(
+        [job],
+        mock_s3,
+        example_bucket_name,
+        mock_ses,
+        example_template_name,
+        example_source_email,
+    )
+
+    assert int(mock_ses.get_send_quota()["SentLast24Hours"]) == 0
+
+
+def test_handler_same_previous_state(
+    helpers,
+    mock_s3: S3Client,
+    mock_ses: SESClient,
+    example_bucket_name,
+    example_template_name,
+    example_source_email,
+    requests_mock,
+):
+    common_setup(
+        helpers,
+        mock_s3,
+        mock_ses,
+        example_bucket_name,
+        example_template_name,
+        example_source_email,
+    )
+
+    job = parse_dict_to_job(helpers.html_monitor_job_dict_factory())
+
+    data = "test"
+    requests_mock.get(job.url, text=data)
+
+    handler(
+        [job],
+        mock_s3,
+        example_bucket_name,
+        mock_ses,
+        example_template_name,
+        example_source_email,
+    )
+
+    handler(
+        [job],
+        mock_s3,
+        example_bucket_name,
+        mock_ses,
+        example_template_name,
+        example_source_email,
+    )
+
+    assert int(mock_ses.get_send_quota()["SentLast24Hours"]) == 0
+
+
+def test_handler_different_previous_state(
+    helpers,
+    mock_s3: S3Client,
+    mock_ses: SESClient,
+    example_bucket_name,
+    example_template_name,
+    example_source_email,
+    requests_mock,
+):
+    common_setup(
+        helpers,
+        mock_s3,
+        mock_ses,
+        example_bucket_name,
+        example_template_name,
+        example_source_email,
+    )
+
+    job = parse_dict_to_job(helpers.html_monitor_job_dict_factory())
+
+    data = "test"
+    requests_mock.get(job.url, text=data)
+
+    handler(
+        [job],
+        mock_s3,
+        example_bucket_name,
+        mock_ses,
+        example_template_name,
+        example_source_email,
+    )
+
+    data = "test-changed"
+    requests_mock.get(job.url, text=data)
+
+    handler(
+        [job],
+        mock_s3,
+        example_bucket_name,
+        mock_ses,
+        example_template_name,
+        example_source_email,
+    )
+
+    assert int(mock_ses.get_send_quota()["SentLast24Hours"]) == 1
